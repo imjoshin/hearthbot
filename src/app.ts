e66cab1c42342e64fa3660dbe5d235dc241a63b7
Josh <jcjohnson77@gmail.com>
2022-07-30 16:45:04 -0400

    mysql init

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,39 @@
+import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
+import mysql from "mysql"
 
-const config = {
-    name: `sample-express-app`,
-    port: 3000,
-    host: `0.0.0.0`,
-}
+dotenv.config()
+
+const pool = mysql.createPool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+})
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 app.get(`/`, (req, res) => {
-    res.status(200).send(`hello world`)
+  res.status(200).send(`hello world`)
 })
 
-app.listen(config.port, config.host)
+app.get(`/test/:id`, (req, res) => {
+  const query = `SELECT * FROM test WHERE id = ?`
+  pool.query(query, [ req.params.id ], (error, results) => {
+    console.log(error)
+    if (!results[0]) {
+      res.json({ status: `Not found!` })
+    } else {
+      res.json(results[0])
+    }
+  })
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
