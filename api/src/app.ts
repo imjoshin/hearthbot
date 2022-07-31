6c2ba745f3129439715740939f6fdad1e969d482
Josh <jcjohnson77@gmail.com>
2022-07-31 14:18:07 -0400

    Pull schema into new file - some msyql bug happening

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,12 +2,11 @@ import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
 import { graphqlHTTP } from "express-graphql"
-import * as graphql from "graphql"
 import path from "path"
 import { LocalDatabase } from "./db/LocalDatabase"
 import { RemoteDatabase } from "./db/RemoteDatabase"
 import { runUpdates } from "./db/schema/util"
-import joinMonster from "join-monster"
+import { createSchema } from "./graphql"
 
 dotenv.config()
 
@@ -30,38 +29,10 @@ const app = async () => {
   app.use(cors())
   app.use(express.json())
 
-  const GraphQLCard = new graphql.GraphQLObjectType({
-    name: `Card`,
-    extensions: {
-      joinMonster: {
-        sqlTable: `card`,
-        uniqueKey: `id`,
-      }
-    },
-    fields: () => ({
-      id: { type: graphql.GraphQLString },
-      name: { type: graphql.GraphQLString },
-    })
-  })
-  
-  const QueryRoot = new graphql.GraphQLObjectType({
-    name: `Query`,
-    fields: () => ({
-      cards: {
-        type: new graphql.GraphQLList(GraphQLCard),
-        resolve: (parent, args, context, resolveInfo) => {
-          return joinMonster(resolveInfo, {}, (sql: string) => {
-            return db.run(sql)
-          })
-        }
-      }
-    })
-  })
-      
-  const schema = new graphql.GraphQLSchema({ query: QueryRoot })
+
   
   app.use(`/`, graphqlHTTP({
-    schema: schema,
+    schema: createSchema(db),
     graphiql: true,
   }))
   
