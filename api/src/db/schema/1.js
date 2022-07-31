899d40b8712c4606901ab21e43f7d029161d8d24
Josh <jcjohnson77@gmail.com>
2022-07-30 23:37:55 -0400

    Get remote builds working again

--- api/src/db/schema/1.js
+++ api/src/db/schema/1.js
@@ -1,6 +1,6 @@
 const run = async (db) => {
   const queries = [
-    `CREATE TABLE config (name TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL, updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, description TEXT, note TEXT );`,
+    `CREATE TABLE config (name VARCHAR(32) NOT NULL PRIMARY KEY, value TEXT NOT NULL, updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, description TEXT, note TEXT );`,
     `INSERT INTO config (name, value) VALUES ("schema", "1")`,
   ]
 
