40a8b828a4a994ffa00c07a96c17d6d489cfe398
Josh <jcjohnson77@gmail.com>
2022-08-10 19:46:25 -0400

    Sync translations in bulk

--- packages/sync/src/sync.ts
+++ packages/sync/src/sync.ts
@@ -1,14 +1,14 @@
 import * as constants from "./constants"
-import { getCache, setCache, api } from "./util"
+import { getCache, setCache, api, objectToGraphqlArgs } from "./util"
 
-export const sync = async (version: string, language: typeof constants.LANGUAGES[number]) => {
-  console.log(`Syncing ${language}: ${version}`)
-  const cacheKey = `cards-${version}-${language}`
+export const sync = async (version: string, locale: typeof constants.LANGUAGES[number]) => {
+  console.log(`Syncing ${locale}: ${version}`)
+  const cacheKey = `cards-${version}-${locale}`
   let cardsJson = getCache(cacheKey)
 
   // fetch card json if we don't have it cached
   if (!cardsJson) {
-    const cardsJsonUrl = `${constants.HEARTHSTONE_JSON_URL}/${version}/${language}/cards.json`
+    const cardsJsonUrl = `${constants.HEARTHSTONE_JSON_URL}/${version}/${locale}/cards.json`
     try {
       const req = await fetch(cardsJsonUrl)
       cardsJson = await req.json()
@@ -19,69 +19,82 @@ export const sync = async (version: string, language: typeof constants.LANGUAGES
     }
   }
 
-  // TODO chunk this up and use createCards
-  let remainingCards = cardsJson.slice(10000, 10950)
+  // chunk cards for batch updating
+  let remainingCards = cardsJson
   while (remainingCards.length) {
     const chunk = remainingCards.slice(0, 100)
-    await syncCards(chunk)
+    await syncCards(locale, chunk)
     remainingCards = remainingCards.slice(100)
   }
 }
 
-const syncCards = async (cards: {[key: string]: any}[]) => {
+const syncCards = async (locale: string, cards: {[key: string]: any}[]) => {
+  const shouldCreateCards = locale === `enUS`
   const cardAttributes = []
+  const cardTranslations = []
+
   for (const card of cards) {
-    const attributes = {
-      attack: card.attack,
-      artist: card.artist,
-      // class: card.cardClass,
-      collectible: !!card.collectible,
-      cost: card.cost,
-      dbfId: card.dbfId,
-      flavor: card.flavor,
-      id: card.id,
-      health: card.health,
-      durability: card.durability,
-      mechanics: card.mechanics ? card.mechanics.join(`,`) : ``,
+    const translations = {
+      cardId: card.id,
       name: card.name,
-      rarity: card.rarity,
-      tribe: card.race,
-      setId: card.set,
       text: card.text,
-      type: card.type,
-    }
-  
-    const graphqlFields: string[] = []
-    
-    for (const key of Object.keys(attributes)) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      const value = attributes[key]
-  
-      if (value !== undefined && value !== null) {
-        graphqlFields.push(`${key}: ${JSON.stringify(value)}`)
-      }
+      flavor: card.flavor,
+      locale: locale,
     }
 
-    cardAttributes.push(`{${graphqlFields.join(`, `)}}`)
+    cardTranslations.push(objectToGraphqlArgs(translations))
+
+    if (shouldCreateCards) {
+      const attributes = {
+        attack: card.attack,
+        artist: card.artist,
+        // class: card.cardClass,
+        collectible: !!card.collectible,
+        cost: card.cost,
+        dbfId: card.dbfId,
+        flavor: card.flavor,
+        id: card.id,
+        health: card.health,
+        durability: card.durability,
+        mechanics: card.mechanics ? card.mechanics.join(`,`) : ``,
+        name: card.name,
+        rarity: card.rarity,
+        tribe: card.race,
+        setId: card.set,
+        text: card.text,
+        type: card.type,
+      }
+
+      cardAttributes.push(objectToGraphqlArgs(attributes))
+    }
   }
   
   try {
+    const createCards = `
+      createCards(
+        cards: [
+          ${cardAttributes.join(`,\n`)}
+        ]
+      ) { success }
+    `
+
+    const createCardTranslations = `
+      createCardTranslations(
+        translations: [
+          ${cardTranslations.join(`,\n`)}
+        ]
+      ) { success }
+    `
+
     const response = await api(`
       mutation {
-        createCards(
-          cards: [
-            ${cardAttributes.join(`\n`)}
-          ]
-        ) { errors }
+        ${shouldCreateCards ? createCards : ``}
+        ${createCardTranslations}
       }
     `)
 
-    const json = await response.json()
-    const {errors} = json.data.createCards
-    if (errors) {
-      console.log(errors)
-    }
+    // const json = await response.json()
+    // console.log(JSON.stringify(json, null, 2))
   } catch (e) {
     console.log(e)
   }
--- packages/sync/src/util.ts
+++ packages/sync/src/util.ts
@@ -29,3 +29,19 @@ export const api = (query: string, variables?: {[key: string]: any}) => {
     body: JSON.stringify({query, variables})
   })
 }
+
+export const objectToGraphqlArgs = (object: {[key: string]: any}): string => {
+  const graphqlFields: string[] = []
+    
+  for (const key of Object.keys(object)) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const value = object[key]
+
+    if (value !== undefined && value !== null) {
+      graphqlFields.push(`${key}: ${JSON.stringify(value)}`)
+    }
+  }
+
+  return `{ ${graphqlFields.join(`, `)} }`
+}
