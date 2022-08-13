07aa4102c755974e6de444f0ad49ebd19c1cbee6
Josh <jcjohnson77@gmail.com>
2022-08-12 23:23:42 -0400

    Get card names for released cards

--- packages/scrape/src/scrape.ts
+++ packages/scrape/src/scrape.ts
@@ -1,3 +1,18 @@
+import { JSDOM } from "jsdom"
+
 export const scrape = async (id: string, scrapeUrl: string) => {
   console.log(`scraping ${id} from ${scrapeUrl}`)
+  
+  const tableUrl = `${scrapeUrl}/?st=&manaCost=&format=wild&rarity=&type=&class=&set=&mechanic=&race=&orderby=DESC-date&view=table`
+  const pageRequest = await fetch(tableUrl)
+  const html = await pageRequest.text()
+  const dom = new JSDOM(html)
+  const root = dom.window.document
+
+  const cardRows = root.querySelectorAll(`tr`)
+  for (const row of cardRows) {
+    const nameLink = row.querySelector(`a`)
+    const cardName = nameLink?.text
+    console.log({cardName})
+  }
 }
