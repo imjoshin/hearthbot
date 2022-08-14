b74a581f907082c7e5c3f448dcd116e7099307d0
Josh <jcjohnson77@gmail.com>
2022-08-13 22:15:01 -0400

    Initial bot setup

--- packages/bot/src/app.ts
+++ packages/bot/src/app.ts
@@ -1,9 +1,34 @@
 import dotenv from "dotenv"
+import Discord from "discord.js"
+import { onCards } from "./events"
 
 dotenv.config()
 
-const run = async() => {
-  console.log(`bot`)
-}
+const client = new Discord.Client({
+  partials: [Discord.Partials.Message, Discord.Partials.Channel, Discord.Partials.Reaction],
+  intents: [
+    `Guilds`,
+    `GuildMessages`,
+    `GuildMessageReactions`,
+    `MessageContent`,
+  ]}
+)
 
-run()
+client.on(`ready`, () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on(`disconnect`, async (erMsg, code) => {
+  console.log(`----- Bot disconnected from Discord with code ` + code + `for reason: ` + erMsg + ` -----`)
+  await new Promise(res => setTimeout(res, 10000))
+  client.login(process.env.DISCORD_CLIENT_TOKEN)
+})
+
+client.on(`messageCreate`, message => {
+  const cards = message.content.match(/\[\[(.*?)\]\]/g)
+  if (cards) {
+    onCards(message, cards)
+  }
+})
+
+client.login(process.env.DISCORD_CLIENT_TOKEN)
---
+++ packages/bot/src/events.ts
@@ -0,0 +1,5 @@
+import { Message } from "discord.js"
+
+export const onCards = (message: Message, cards: string[]) => {
+  console.log(cards)
+}
